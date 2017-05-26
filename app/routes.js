068ec7c1498d7be38a48533e85e6198661b36684
allisonkuo <aly.kuo@gmail.com>
2017-05-25 19:42:41 -0700

    added create company page front-end elements

--- app/routes.js
+++ app/routes.js
@@ -60,6 +60,17 @@ module.exports = function(app, passport) {
     });
 
     // =====================================
+    // CREATE COMPANY ======================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/createcompany', isLoggedIn, function(req, res) {
+        res.render('createcompany.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
     // LOGOUT ==============================
     // =====================================
     app.get('/logout', function(req, res) {
